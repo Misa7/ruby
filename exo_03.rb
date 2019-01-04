8dff2cca6a1392dcab868daf4009c6752631ff59
Misa7 <ma.ducros7@gmail.com>
2019-01-04 19:42:08 +0100

    add exo 4

--- exo_03.rb
+++ exo_03.rb
@@ -1,2 +1,4 @@
 puts "Bonjour, monde !"
 #puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+	#toute ligne commençant par "#" est un comment
---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ça farte ?"
+#pas de " à la fin de la formule donc msg d'erreur unterminated string meets end of file
+
