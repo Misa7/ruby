88049abc6f2663ca5aaca9ca173a73b70f51a013
Misa7 <ma.ducros7@gmail.com>
2019-01-05 09:01:38 +0100

    modif pyramide

--- pyramide.rb
+++ pyramide.rb
@@ -1,10 +1,11 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 i = gets.chomp.to_i
-a = 0
-b = "#"
-c = i.to_i
+a = ""
+b = " "
+c = b * i
 
 puts "Voici la pyramide :"
-c.times do
-	puts b * (a -= 1)
+i.times do
+	puts b * (i -= 1) + "#{a += "#"}"
 end
+
